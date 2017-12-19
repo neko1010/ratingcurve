1207259d127cb775fe4bba98504108e76bfe9b06
Nick Kolarik <nicholaskolarik@gmail.com>
2017-12-19 11:52:25 -0500

    "All stage/Q data sorted for 3 Green River sites"

--- curve.sh
+++ curve.sh
@@ -1,7 +1,10 @@
 for file in $@
 	do 
 		echo $file
-		grep ^USGS $file | cut -f 4,9,10 | sort -rnk4 | head -5
+		grep ^USGS $file | cut -f 4,9,10 | sort -rnk4 > ${file/%.txt/_out.txt} 
+#https://www.biostars.org/p/148345://www.biostars.org/p/148345/
 		echo
 	done
+	
 # Unsure why, but sort -rnk3 sorts by the second key/column and 4 the third
+# Removed head, sent output to file
